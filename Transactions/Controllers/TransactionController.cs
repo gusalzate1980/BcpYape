fc3a631cdf35468908de09125515b4944733f4c2
gusalzate1980 <gusalzate1980@gmail.com>
2025-04-02 10:46:41 -0500

    fix 2

---
+++ Transactions/Controllers/TransactionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Transactions.BusinessLogic;
+using Transactions.Dto;
+using Transactions.Kafka;
+
+namespace Transactions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly TransactionKafka _kafka;
+
+        public TransactionController(TransactionKafka kafka)
+        {
+            _kafka = kafka;
+        }
+
+        [HttpPost]
+        public IActionResult AddTransaction(AddTransferDto dto)
+        {
+            TransactionBl transactionBl = new TransactionBl(_kafka);
+            return Ok(transactionBl.AddTransaction(dto));
+        }
+    }
+}
